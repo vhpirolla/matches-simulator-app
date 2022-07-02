66add4e3100c02584fbd4a62b695022902ea7f3e
vPirolla <vitorpirolla@gmail.com>
2022-07-02 11:32:41 -0300

    Restore MainActivity

--- app/src/main/java/com/example/simulator/MainActivity.kt
+++ app/src/main/java/com/example/simulator/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.simulator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class git status
-MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
