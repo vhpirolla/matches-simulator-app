6b264a7606d9d4cb3a29676e19730c2471c33ab0
vPirolla <vitorpirolla@gmail.com>
2022-07-02 11:09:56 -0300

    first commit

---
+++ app/src/main/java/com/example/simulator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class git status
+MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
