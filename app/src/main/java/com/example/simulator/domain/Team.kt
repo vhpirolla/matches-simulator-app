4b3b42f2aee11abb973d0f4dfae85fbd42494e72
vPirolla <vitorpirolla@gmail.com>
2022-07-16 18:07:53 -0300

    Finalização do APP de Simulação de Partidas

---
+++ app/src/main/java/com/example/simulator/domain/Team.kt
@@ -0,0 +1,16 @@
+package com.example.simulator.domain
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Team (
+    @SerializedName("nome")
+    val name: String,
+    @SerializedName("estrelas")
+    val stars: Int,
+    @SerializedName("imagem")
+    val image: String,
+    var score: Int?
+) : Parcelable
